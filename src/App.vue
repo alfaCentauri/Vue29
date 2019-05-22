9ba4270be401826526008f6f61ea190057af1292
Ricardo Presilla <ricardopresilla@gmail.com>
2019-05-22 12:09:41 -0400

    Creado el App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <ListaProyectos></ListaProyectos>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ListaProyectos from './components/proyectos'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ListaProyectos
+  }
+}
+</script>
+
+<style>
+
+</style>
